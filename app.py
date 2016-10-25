56995773147c3b374b1c04d91e45270159402517
Marundu <marundu@gmail.com>
2016-10-25 18:49:38 +0300

    Improved text visibility

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route('/')
 def current_day_and_time():
-	time_now=time.strftime('%I:%M%p %Z')
+	time_now=time.strftime('%I:%M %p %Z')
 	day_today=time.strftime('%A, %B %d, %Y')
 	return render_template('time.html', time_now=time_now, day_today=day_today)
 
