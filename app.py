f2add9ff69a4a9dafa565c55a4f131771fc8fb26
Marundu <marundu@gmail.com>
2017-02-27 11:54:02 +0300

    Style improvements

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 import time
 
 app=Flask(__name__)
@@ -7,7 +8,11 @@ app=Flask(__name__)
 def current_day_and_time():
 	time_now=time.strftime('%I:%M %p %Z')
 	day_today=time.strftime('%A, %B %d, %Y')
-	return render_template('time.html', time_now=time_now, day_today=day_today)
+	
+	return render_template('time.html',
+	    time_now=time_now,
+	    day_today=day_today
+	    )
 
 if __name__=='__main__':
 	app.run(debug=True, port=7096)
